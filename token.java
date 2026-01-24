01e215682c60784b89ea58e1f06a53e216e2e82a
Ajay Patil <patilaajay452@gmail.com>
2026-01-24 22:55:11 +0530

    Add files via upload
    token explain with exampal

---
+++ token.java
@@ -0,0 +1,15 @@
+class token
+{
+    public static void main(String[]args)
+    {
+        int a=20,b=30;
+        int n=a+b;
+        System.out.print(n);
+    }
+}
+
+//  tokens :
+//  i) keywords :- int,static,void,etc;
+//  ii) identifire:- variable name,class name, package name,etc;
+//  iii) operator :- +,-,=,*,/,;
+//  iv) sepretor:- ; , . : {} ();
