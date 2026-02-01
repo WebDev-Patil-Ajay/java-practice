000ba14ab0f62a4317c32fd2dcce912e5daa0ccd
Ajay Patil <patilaajay452@gmail.com>
2026-02-01 21:59:26 +0530

    Add files via upload
    = → value देतो
    
    += → add करून देतो
    
    -= → minus करून देतो
    
    *= → multiply
    
    /= → divide
    
    %= → remainder

---
+++ Assignment.java
@@ -0,0 +1,21 @@
+class Assignment {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        a += 5;
+        System.out.println(a); // 15
+
+        a -= 3;
+        System.out.println(a); // 12
+
+        a *= 2;
+        System.out.println(a); // 24
+
+        a /= 4;
+        System.out.println(a); // 6
+
+        a %= 5;
+        System.out.println(a); // 1
+    }
+}
