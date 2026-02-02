c62b50d7c542a2b08246afaa5e5a2ecdcdd4f1e0
Ajay Patil <patilaajay452@gmail.com>
2026-02-02 23:08:26 +0530

    Add files via upload
    Operator	काय होतं
    ++a	आधी वाढ
    a++	नंतर वाढ
    --a	आधी कमी
    a--	नंतर कमी

---
+++ IncrDcrement.java
@@ -0,0 +1,25 @@
+class IncrDcredirement
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+
+        System.out.println("Initial value a = " + a);
+
+        // Post Increment
+        System.out.println("a++ = " + a++);
+        System.out.println("After a++ , a = " + a);
+
+        // Pre Increment
+        System.out.println("++a = " + ++a);
+
+        // Post Decrement
+        System.out.println("a-- = " + a--);
+        System.out.println("After a-- , a = " + a);
+
+        // Pre Decrement
+        System.out.println("--a = " + --a);
+
+        System.out.println("Final value a = " + a);
+    }
+}
