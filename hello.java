436d8c778ee3635f2d3859e06e40573b0c30301c
Ajay Patil <patilaajay452@gmail.com>
2026-02-16 22:40:54 +0530

    Add files via upload
    hotel Management

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
-class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+class hello
+  {
+    public static void main(String args[])
+    {
+        System.out.print("hello world");
     }
-}
+  }
