10391ceb6cccd3b6aa3369d53750eba11aef7819
Ajay Patil <patilaajay452@gmail.com>
2026-02-21 00:01:03 +0530

    Add files via upload
    prnt 1 to 10 revars

---
+++ looptray2.java
@@ -0,0 +1,12 @@
+class looptray2
+{
+    public static void main(String[]args)
+    {
+        int i;
+        for(int i = 10; i >= 1; i--)
+        {
+            System.out.println("printl 1 to 10 revers number");
+            System.out.println(i);
+        }
+    }
+}
