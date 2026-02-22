b04fdcf1e075c0478ef6724f26e2804ab3a5770e
Ajay Patil <patilaajay452@gmail.com>
2026-02-22 23:05:15 +0530

    Add files via upload
    odd number

---
+++ whileloopodd.java
@@ -0,0 +1,16 @@
+class whileloopodd
+{
+    public static void main(String[]args)
+    {
+        int num=20;
+        int i=0;
+        while(i<=num)
+        {
+            if(i%2 !=0)
+            {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
