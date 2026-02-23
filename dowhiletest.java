f1f8e1f3c2083ccf44a23f9a16970354e6c7d480
Ajay Patil <patilaajay452@gmail.com>
2026-02-23 23:29:27 +0530

    Add files via upload
    1 to 5 numbers print using do while loop .dail learn one loop consept.

---
+++ dowhiletest.java
@@ -0,0 +1,14 @@
+class dowhiletest
+{
+    public static void main(String[] args)
+    {
+        int i = 1;
+
+        do
+        {
+            System.out.println(i);
+            i++;
+        }
+        while(i <= 5);
+    }
+}
