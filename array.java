ed5a76c83518b265a98587f200f2c9fc564f68af
Ajay Patil <patilaajay452@gmail.com>
2026-03-01 22:08:39 +0530

    Add files via upload
    Array declaretion access value

---
+++ array.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class array
+{
+    public static void main(String[] args)
+    {
+        int[] marks = {70, 80, 90};
+        String[] names = {"Ajay", "Rohit", "Sagar"};
+
+        System.out.println(names[0] + " : " + marks[0]);
+        System.out.println(names[1] + " : " + marks[1]);
+        System.out.println(names[2] + " : " + marks[2]);
+    }
+}
