bbdc967f5e0fe296a6490b28944ef6c4d2c36087
Ajay Patil <patilaajay452@gmail.com>
2026-03-08 23:09:27 +0530

    Add files via upload

---
+++ arraindex.java
@@ -0,0 +1,11 @@
+class SimpleArray
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = {10, 20, 30};
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
