0b58e6fe10fe4531bdfc38f9415d7dca67d73a96
Ajay Patil <patilaajay452@gmail.com>
2026-03-10 23:36:09 +0530

    Add files via upload
    Added basic array programs in Java

---
+++ studentmarks.java
@@ -0,0 +1,12 @@
+class Studentmarks
+{
+    public static void main(String[] args)
+    {
+        int[] marks = {70, 80, 65, 90, 75};
+
+        for(int i = 0; i < marks.length; i++)
+        {
+            System.out.println("Student " + (i+1) + " Marks : " + marks[i]);
+        }
+    }
+}
